dd3e7b1310ef63e245d9a64eb3f7f2e700e1338f
Hu <leohu@deloitte.com.cn>
2019-09-03 11:55:57 +0800

    修复bug

--- src/main/java/com/eyeieye/melody/demo/DemoBootApplication.java
+++ src/main/java/com/eyeieye/melody/demo/DemoBootApplication.java
@@ -1,10 +1,13 @@
 package com.eyeieye.melody.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.eyeieye.melody"})
 @ImportResource(locations = { "classpath:service-beans.xml" })
