f9eb3d9afe918def1544c720558017a580492f6a
liuxuetang <xuetangok@163.com>
2019-09-04 10:12:43 +0800

    配置优化

--- src/main/java/com/eyeieye/melody/demo/conf/ResolverConfiguration.java
+++ src/main/java/com/eyeieye/melody/demo/conf/ResolverConfiguration.java
@@ -4,16 +4,18 @@ import com.eyeieye.melody.demo.controller.UserArgumentResolver;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+/**
+ * 注意：若通过继承WebMvcConfigurationSupport方式实现配置，可能会覆盖默认配置，导致LCN分布式事务失效。
+ */
 @Configuration
-public class ResolverConfiguration  extends WebMvcConfigurerAdapter {
+public class ResolverConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new UserArgumentResolver());
-        super.addArgumentResolvers(argumentResolvers);
     }
 }
