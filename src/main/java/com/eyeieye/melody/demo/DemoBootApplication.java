8a3c182365cd0a522222c3a580665c47a4559010
liuxuetang <xuetangok@163.com>
2019-09-09 18:31:36 +0800

    静态资源服务配置

--- src/main/java/com/eyeieye/melody/demo/DemoBootApplication.java
+++ src/main/java/com/eyeieye/melody/demo/DemoBootApplication.java
@@ -1,21 +1,15 @@
 package com.eyeieye.melody.demo;
 
 import com.eyeieye.melos.MelosConfig;
-import com.eyeieye.melos.MelosMvcConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication()
-@ComponentScan(basePackages = { "com.eyeieye.melos.profiler","com.eyeieye.melos.rpc","com.eyeieye.melos.util","com.eyeieye.melos.web","com.eyeieye.melody.demo"},
-basePackageClasses = MelosConfig.class
-)
+@ComponentScan(basePackages = {"com.eyeieye.melos.*", "com.eyeieye.melody.demo"}, basePackageClasses = MelosConfig.class)
 @ImportResource(locations = { "classpath:service-beans.xml" })
 public class DemoBootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBootApplication.class, args);
 	}
--- src/main/java/com/eyeieye/melody/demo/conf/StaticViewConfig.java
+++ src/main/java/com/eyeieye/melody/demo/conf/StaticViewConfig.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class StaticViewConfig extends MelosMvcConfig  {
@@ -16,9 +15,14 @@ public class StaticViewConfig extends MelosMvcConfig  {
 	@Autowired
 	private ResourceProperties resourceProperties;
 
+	/**
+	 * 继承WebMvcConfigurationSupport会覆盖默认配置，此处重新配置静态资源访问
+	 * @param registry
+	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		super.addResourceHandlers(registry);
-		registry.addResourceHandler(properties.getStaticPathPattern()).addResourceLocations(resourceProperties.getStaticLocations());
+		registry.addResourceHandler(properties.getStaticPathPattern())
+				.addResourceLocations(resourceProperties.getStaticLocations());
 	}
 }
